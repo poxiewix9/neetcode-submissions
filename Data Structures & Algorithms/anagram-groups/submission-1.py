051cd6b33a6116229af1083986fe1738dfbc0aa0
Keshav Singh <96786678+poxiewix9@users.noreply.github.com>
2026-08-25 19:22:42 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        solution = defaultdict(list)
+
+        for s in strs:
+            letterFreq = [0] * 26
+            for char in s:
+                letterFreq[ord(char) - ord('a')] += 1
+            solution[tuple(letterFreq)].append(s)
+        
+        return list(solution.values())
